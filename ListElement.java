2614720a8b44e10574ff3e563a429c0ae636b691
regonasg <43829846+regonasg@users.noreply.github.com>
2018-10-23 10:39:49 -0600

    Add files via upload
    creating a Linked List Element and set it all to null

---
+++ ListElement.java
@@ -0,0 +1,13 @@
+
+public class ListElement {
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+}
