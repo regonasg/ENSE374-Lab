3b4e13805e6d8f6f728b79950cb9f7eb8bf26e0e
regonasg <43829846+regonasg@users.noreply.github.com>
2018-10-23 19:36:20 -0600

    deleted "private ListElement previous"
    not relevant for a single linked list

--- ListElement.java
+++ ListElement.java
@@ -1,14 +1,12 @@
 
 public class ListElement {
 	private ListElement next;
-	private ListElement previous;
 	private int data;
 	
 	public ListElement()
 	{
 		this.data = 0;
 		this.next = null;
-		this.previous = null;
 	}
 	
 	public void setData (int data)
